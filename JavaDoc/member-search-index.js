5f82acbefcf328936c577d0242e1649c13b9c6d7
ivans <ivan.sharapov.05@yandex.ru>
2025-05-05 11:14:11 +0300

    DEL JavaDoc

--- JavaDoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"Exceptions","c":"EmptySearchException","l":"EmptySearchException()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Main","l":"show()"}];updateSearchResults();
